b4eb50ff97e534f78b3c89efaf006ba6d1f4fb45
josh <josch>
2022-02-08 09:00:21 +0100

    Initial Commit

---
+++ Final/FinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalApp.swift
+//  Final
+//
+//  Created by Slawomir on 08/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct FinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
